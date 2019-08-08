ed8ba5d23e972d89c7148d9f6f4cc949207d1972
Alexandre Araujo <bryansimones50@gmail.cols>
2019-08-07 20:39:34 -0400

    funcao subtrair

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,7 @@
-"" 
+function subtrair(){
+                var valor1 = document.getElementById("n1").value; 
+                var valor2 = document.getElementById("n2").value;
+                var subtracao = valor1 - valor2;
+            
+                alert(subtracao);
+            }
