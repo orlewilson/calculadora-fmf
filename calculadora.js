c07b58a98e8ca37d5db6ba58af3671b42c8792cb
Paulo H. Machado <ph_machado@live.com>
2019-08-07 20:56:06 -0400

    Metodo divisao

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,6 @@
-"" 
+function dividirValores(){
+	var parseint1 = document.getElementById("n1").value;
+	var parseint2 = document.getElementById("n2").value;
+    var x = parseint1/parseint2;
+    alert(x);
+}
