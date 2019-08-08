bb65a9976f852606b3f1997b4dedc3209677264e
helenilson Oliveira <helenilsoon@gmail.com>
2019-08-07 21:10:04 -0400

    função da soma

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,6 @@
-"" 
+function soma(){	
+	var n1 = parseFloat(document.getElementById("n1").value);
+	var n2 = parseFloat(document.getElementById("n2").value);
+	var resultado = n1 + n2;
+     alert(resultado);
+}
