b8ded60e9e762154c418854a8f0e1e9c2c77226d
ABNER JUDÁ CAMPOS VASCONCELOS <abnercampos446@gmail.com>
2019-08-07 21:11:40 -0400

    metodo multiplacar

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,8 @@
-"" 
+function multiplicar() {
+   var valor1 = document.getElementById("n1").value;
+   var valor2 = document.getElementById("n2").value;
+
+   var res = valor1*valor2;
+	alert(res);
+}
+
