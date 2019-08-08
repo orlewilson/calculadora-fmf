b075a06f0934b2adbca4767b30b9006c7ee0705a
helenilson Oliveira <helenilsoon@gmail.com>
2019-08-07 21:23:02 -0400

    espaço para nao da erro

--- calculadora.js
+++ calculadora.js
@@ -1,3 +1,30 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function soma(){	
 	var n1 = parseFloat(document.getElementById("n1").value);
 	var n2 = parseFloat(document.getElementById("n2").value);
